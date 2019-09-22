ab960e6d5eaa9eaf85d2719cd72361e2bc0e0c3e
Zach Stott <spywoawe@gmail.com>
2019-09-23 11:06:33 +1200

    Basic axe spawn

--- New Unity Project/Assets/Scripts/axe.cs
+++ New Unity Project/Assets/Scripts/axe.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BigJump : MonoBehaviour
+public class axe : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -13,6 +13,6 @@ public class BigJump : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += Vector3.right * Time.deltaTime * 15;
     }
 }
