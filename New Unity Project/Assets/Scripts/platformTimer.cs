d883f8f4a03b4e1dbac2bf766f9b433d032e2c4f
Zach Stott <spywoawe@gmail.com>
2019-09-23 11:40:13 +1200

    Broken coroutine

---
+++ New Unity Project/Assets/Scripts/platformTimer.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class platformTimer : MonoBehaviour
+{
+    public GameObject set1;
+    public GameObject set2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine("platformSwitch");
+        //InvokeRepeating("platform1", 0, 0);
+        // InvokeRepeating("platform2", 0, 0);
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator platformSwitch()
+    {
+        while (true)
+        {
+            new WaitForSeconds(2);
+            set1.SetActive(false);
+            set2.SetActive(true);
+
+            yield return new WaitForSeconds(2);
+            set1.SetActive(true);
+            set2.SetActive(false);
+        }
+    }
+    
+
+    void platform1()
+    {
+        set1.SetActive(true);
+        new WaitForSeconds(2);
+        set1.SetActive(false);
+        new WaitForSeconds(2);
+    }
+
+    void platform2()
+    {
+        set2.SetActive(false);
+        new WaitForSeconds(2);
+        set2.SetActive(true);
+        new WaitForSeconds(2);
+    }
+}
